8bc77699978ae67ede80a92b2cca983595610281
AndreyCorpse28090 <280990corpse@gmail.com>
2025-11-12 20:28:01 +0300

    refactor(db): update Note model, remove UserId, add UpdatedAt and SyncedAt

--- src/CloudNotes.Desktop/Program.cs
+++ src/CloudNotes.Desktop/Program.cs
@@ -1,9 +1,7 @@
 using Avalonia;
 using System;
-using Microsoft.EntityFrameworkCore;
-using CloudNotes.Desktop.Data;
 
-namespace CloudNotes.Desktop;
+namespace CloudNotes;
 
 class Program
 {
@@ -11,23 +9,8 @@ class Program
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args)
-    {
-        // Create DbContextOptionsBuilder for configuring the connection
-        var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseSqlite($"Data Source={Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}/CloudNotes/notes.db");
-
-        // Create context and call EnsureCreated
-        using (var context = new AppDbContext(optionsBuilder.Options))
-        {
-            context.Database.EnsureCreated();
-        }
-
-        // Launch the Avalonia application
-        BuildAvaloniaApp()
-            .StartWithClassicDesktopLifetime(args);
-    }
-
+    public static void Main(string[] args) => BuildAvaloniaApp()
+        .StartWithClassicDesktopLifetime(args);
 
     // Avalonia configuration, don't remove; also used by visual designer.
     public static AppBuilder BuildAvaloniaApp()
