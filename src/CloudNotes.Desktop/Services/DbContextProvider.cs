5aeaceb19df88900c54ef15916713d58b2d4341c
AndreyCorpse28090 <280990corpse@gmail.com>
2025-11-12 20:28:01 +0300

    refactor(db): update Note model, remove UserId, add UpdatedAt and SyncedAt

---
+++ src/CloudNotes.Desktop/Services/DbContextProvider.cs
@@ -0,0 +1,23 @@
+using System;
+using CloudNotes.Desktop.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace CloudNotes.Services;
+
+public static class DbContextProvider
+{
+    private static AppDbContext? _context;
+
+    public static AppDbContext GetContext()
+    {
+        if (_context == null)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlite($"Data Source={Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}/CloudNotes/notes.db");
+            _context = new AppDbContext(optionsBuilder.Options);
+            _context.Database.EnsureCreated(); // Creates a database on the first access
+        }
+
+        return _context;
+    }
+}
