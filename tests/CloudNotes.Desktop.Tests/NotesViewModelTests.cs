1b3b7f868da52b4b1b3d5c13284170ace49a9ead
Вячеслав <rayn_gosling@MacBook-Pro-Vaceslav.local>
2025-11-15 19:07:12 +0300

    refactor: Rid of magic numbers in create note button test

--- tests/CloudNotes.Desktop.Tests/NotesViewModelTests.cs
+++ tests/CloudNotes.Desktop.Tests/NotesViewModelTests.cs
@@ -1,23 +1,41 @@
 using System;
 using Xunit;
 using CloudNotes.Desktop.ViewModel;
+using Avalonia.Diagnostics;
 
 namespace CloudNotes.Desktop.Tests
 {
     public class NotesViewModelTests
     {
-        [Fact]
-        public void CreateNote_AddsNoteToCollectionAndSelectsIt()
+        private readonly NotesViewModel vm;
+
+        public NotesViewModelTests()
         {
-            var vm = new NotesViewModel();
-
-            vm.CreateNote();
-
-            Assert.Equal(3, vm.AllNotes.Count);
-            Assert.Equal(3, vm.Notes.Count);
-            Assert.NotNull(vm.SelectedListItem);
-            Assert.NotNull(vm.SelectedNote);
-            Assert.Equal(vm.SelectedListItem!.Id, vm.SelectedNote!.Id);
+            vm = new NotesViewModel();
         }
-    }
-}
+
+        public class CreateNote : NotesViewModelTests
+        {
+            [Fact]
+            public void IncreasesNotesCount()
+            {
+                var initialCount = vm.AllNotes.Count;
+
+                vm.CreateNote();
+
+                Assert.Equal(initialCount + 1, vm.AllNotes.Count);
+                Assert.Equal(initialCount + 1, vm.Notes.Count);
+            }
+
+            [Fact]
+            public void SelectsNewlyCreatedNote()
+            {
+                vm.CreateNote();
+
+                Assert.NotNull(vm.SelectedListItem);
+                Assert.NotNull(vm.SelectedNote);
+                Assert.Equal(vm.SelectedListItem!.Id, vm.SelectedNote!.Id);
+            }
+        } // class CreateNote        
+    } // class NotesViewModelTests
+} // namespace CloudNotes.Desktop.Tests
